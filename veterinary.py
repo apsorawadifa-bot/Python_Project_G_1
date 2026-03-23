24c1c3bc28fe2b9af165ef2f801b7fc4b28b651f
mstnahidalizaaa <25-62195-2@student.aiub.edu>
2026-03-23 18:58:35 +0600

    Update veterinary.py

--- veterinary.py
+++ veterinary.py
@@ -26,6 +26,9 @@ class Veterinary:
         Buttonframe=Frame(self.root,bd=20,relief=RIDGE)
         Buttonframe.place(x=0,y=515,width=800,height=70)
 
+        global text_Pet_Name, text_Owner_Name, text_Phone
+        global pet_type_var, sex_var, vaccinated_var
+
 
         label_Pet_Name=Label(Dataframe,text="Pet Name:",font=("Calibri (Body)",13,"bold"),padx=2,pady=6)
         label_Pet_Name.grid(row=0,column=0,sticky=W)
@@ -82,8 +85,36 @@ class Veterinary:
         button_exit=Button(Buttonframe,text="Exit",bg="green",fg="white",font=("Calibri (Body)",13,"bold"),width=18)
         button_exit.grid(row=0,column=3)
 
+    def update_info(self):
+         update_window = Toplevel()
+         update_window.title("Update Information")
+         update_window.geometry("600x400")
+
+         pet_name = text_Pet_Name.get()
+         owner_name = text_Owner_Name.get()
+         phone = text_Phone.get()
+         pet_type = pet_type_var.get()
+         sex = sex_var.get()
+         vaccinated = vaccinated_var.get()
+
+        
+
+         Label(update_window, text="Pet Name: " + pet_name,font=("Calibri (Body)",13,"bold"),padx=2,pady=6).pack()
+         Label(update_window, text="Owner Name: " + owner_name,font=("Calibri (Body)",13,"bold"),padx=2,pady=6).pack()
+         Label(update_window, text="Phone: " + phone,font=("Calibri (Body)",13,"bold"),padx=2,pady=6).pack()
+         Label(update_window, text="Pet Type: " + pet_type,font=("Calibri (Body)",13,"bold"),padx=2,pady=6).pack()
+         Label(update_window, text="Sex: " + sex,font=("Calibri (Body)",13,"bold"),padx=2,pady=6).pack()
+         Label(update_window, text="Vaccinated: " + vaccinated,font=("Calibri (Body)",13,"bold"),padx=2,pady=6).pack()
+             
+
+#======================close Button========================
+
+         Button(update_window, text="Close", font=("Arial", 12), bg="red", fg="white",command=update_window.destroy,width=18).pack(pady=20)
+
+#=====================Confirm Button========================    
     
-         
+         Button(update_window,text="Confirm",font=("Arial", 12),bg="green",fg="white",command=lambda: [messagebox.showinfo("Success", "Submit Successfully ✅"), update_window.destroy()],width=18).pack(pady=40)
+
 
 root=Tk() 
 ob=Veterinary(root)
